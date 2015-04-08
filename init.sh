ce72cd33a3f2859a0b381cfe7bb8910aa2326f58
hira-yahoo <misocasu@yahoo.co.jp>
2015-04-08 10:15:57 +0900

    add two shells to configure.

---
+++ init.sh
@@ -0,0 +1,6 @@
+alias echo_current_owner='git config --global user.name; git config --global user.email;'
+
+ROOT=`pwd`
+alias go_to_alice='cd $ROOT/users/alice/; git config --global user.name "Alice"; git config --global user.email alice@example.com'
+alias go_to_bob='cd $ROOT/users/bob/; git config --global user.name "Bob"; git config --global user.email bob@example.com'
+alias reset_user='cd $ROOT; sh reset.sh'
---
+++ reset.sh
@@ -0,0 +1,4 @@
+git config --global user.name "hira-yahoo"
+git config --global user.email misocasu@yahoo.co.jp
+
+
